118006d19f1aa6748fb7308d0ae6b8d41e6d39e0
quanta2006 <quanta2006@163.com>
2017-04-26 09:07:54 +0800

    增加自动打开浏览器/图片压缩/代码压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,33 @@
+var webpack = require('webpack');
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
-  entry: './src/sys/js/index.js',
-  output: {
-    filename: './dist/sys.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-  ],
-  module: {
-    loaders:[
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}
-    ]
-  }
+    entry: './src/sys/js/index.js',
+    output: {
+        filename: './dist/sys.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // html代码热加载
+            template: './src/index.html'
+        }),
+        new OpenBrowserPlugin({ //自动打开浏览器
+            url: 'http://localhost:8080'
+        }),
+        new uglifyJsPlugin({ //js代码压缩
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    module: {
+        loaders: [{
+            test: /\.less$/,   // less解析器
+            loader: 'style-loader!css-loader!less-loader'
+        }, {
+            test: /\.(png|jpg)$/,  // img压缩器
+            loader: 'url-loader?limit=8192'
+        }, ]
+    }
 };
