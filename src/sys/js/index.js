aaab7cd9d30ae3f3ccd88413323271827994b404
quanta2006 <quanta2006@163.com>
2017-05-18 14:58:28 +0800

    自动公共包编译

--- src/sys/js/index.js
+++ src/sys/js/index.js
@@ -1,8 +1,16 @@
-import '../less/index.less'
+require('jquery')
+require('../less/index.less')
+
+var moment = require('moment')
+console.log(moment().format())
 
 $(init)
 
 function init () {
     console.log('ccc')
-    console.log('aaa')
+    console.log('this is today')
+
+    $('.btn-primary').on('click', function () {
+        self.location = 'main.html'
+    })
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,53 @@ var webpack = require('webpack');
 var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var path = require("path");
+var ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+var WebpackChunkHash = require("webpack-chunk-hash");
+var { BundleAnalyzerPlugin }  = require('webpack-bundle-analyzer')
 
 module.exports = {
-    entry: './src/sys/js/index.js',
+    entry: { 
+        main: './src/sys/js/index.js',
+        vendor: ['moment', 'jquery']
+    },
     output: {
-        filename: './dist/sys.js'
+        path: path.join(__dirname, "dist"),
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+        // new BundleAnalyzerPlugin(),
         new HtmlWebpackPlugin({ // html代码热加载
             template: './src/index.html'
         }),
         new OpenBrowserPlugin({ //自动打开浏览器
             url: 'http://localhost:8080'
         }),
-        // new uglifyJsPlugin({ //js代码压缩
-        //     compress: {
-        //         warnings: false
-        //     }
-        // })
+        new webpack.ProvidePlugin({ //jquery解析器
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new webpack.optimize.CommonsChunkPlugin({  //公共库抽取
+            name: ["vendor", "manifest"], // vendor libs + extracted manifest
+            minChunks: Infinity,
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        new WebpackChunkHash(),
+        new ChunkManifestPlugin({
+          filename: "chunk-manifest.json",
+          manifestVariable: "webpackManifest"
+        }),
+        new uglifyJsPlugin({ //js代码压缩
+            compress: {
+                warnings: false
+            }
+        })
     ],
     module: {
         rules: [
         {
-            test: /\.js$/,
+            test: /\.js$/,   //eslint语法解析
             exclude: /node_modules/,
             loader: 'eslint-loader',
             enforce: 'pre',
